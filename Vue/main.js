ad913831c5ad28d5c28822da732fa1a71adb0a10
chalestristian <thales.cristianeugenio@gmail.com>
2021-10-12 23:29:19 -0300

    v-bind and more

--- Vue/main.js
+++ Vue/main.js
@@ -1,7 +1,15 @@
 const app = Vue.createApp({
-    data() {
+    data(){
         return {
-            product: "Dark Chocolate"
+            product: 'Dark Chocolate',
+            image: './assets/images/dark_choc.jpg',
+            link: 'https://google.com.br',
+            stock: 5,
+            details:['60% cocoa', '30% sugar', '10% chocolate butter'],
+            variants: [
+                {id:'2234', type:'Dark'},
+                {id:'4564', type:'White'}
+            ]
         }
     }
 })
